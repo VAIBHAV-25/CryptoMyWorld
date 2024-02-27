1dc94dbcfe539b54ea11ecf6654e0a9b96a7838f
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2024-02-27 12:25:51 +0530

    nam changee

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto My World
+            CryptoPJ
           </Typography>
           <Typography
             variant="subtitle2"
